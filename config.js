c55d3c9348ff465c807affe33cd2275110d0d842
emir-oss <emiremirkilic34@gmail.com>
2025-05-07 18:53:39 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    token: "", // Discord bot tokeninizi buraya girin
-    prefix: "n.", // Bot prefix'ini buraya girin
-    botStatus: "Tate No Yuuusha", // Bot durumunu buraya girin
-    ownerID: "", // Bot sahibinin Discord ID'sini buraya girin
-    logChannelId: "", // Log kanalının ID'sini buraya girin
-    website: "https://oktaydev.online", // Bot web sitesi URL'si (opsiyonel)
+    token: "MTM2OTAzMjI4NzYxNzk0NTY1MQ.GtpQrj.Xa_pIzEcYexz_iVq2-KvaODZ0vr9kB158Taqt0", // Discord bot tokeninizi buraya girin
+    prefix: "e.", // Bot prefix'ini buraya girin
+    botStatus: "ElysionMC", // Bot durumunu buraya girin
+    ownerID: "731213621425602608", // Bot sahibinin Discord ID'sini buraya girin
+    logChannelId: "1369357763783819364", // Log kanalının ID'sini buraya girin
+    website: "-", // Bot web sitesi URL'si (opsiyonel)
     embedColor: "#5865F2", // Varsayılan embed rengi
     embedErrorColor: "#ED4245", // Hata embedlerinin rengi
     embedSuccessColor: "#57F287", // Başarı embedlerinin rengi 
@@ -17,7 +17,7 @@ module.exports = {
     dailyCoins: 500, // Günlük olarak alınabilecek coin miktarı
     
     autoModEnabled: true, // Otomatik moderasyon sistemini aktif/pasif yap
-    maxWarnings: 3, // Maksimum uyarı sayısı
+    maxWarnings: 7, // Maksimum uyarı sayısı
     
     debug: false, // Debug modunu aktif/pasif yap
     maintenance: false // Bakım modunu aktif/pasif yap
